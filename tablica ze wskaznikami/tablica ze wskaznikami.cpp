e32017594966e9ba10178628cc47025018e64070
igors2004 <is310810@student.polsl.pl>
2024-04-24 01:32:37 +0200

    tablicawskazniki

---
+++ tablica ze wskaznikami/tablica ze wskaznikami.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+
+float srednia(float* tab, int ile)
+{
+	float suma = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		suma += *tab;
+		tab++;
+	}
+	return suma / ile;
+}
+
+
+int main()
+{
+	float tablica[3] = { 1.5, 2.3, 2.8 };
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << tablica[i] << endl;
+	}
+	cout << "srednia liczb z tablicy wynosi: " << srednia(tablica, 3);
+}
